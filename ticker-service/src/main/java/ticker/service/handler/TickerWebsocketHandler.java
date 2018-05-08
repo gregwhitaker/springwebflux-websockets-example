321e293ea5733540e5171769ed845e4e7ba1f3af
gregwhitaker <gwhitake@gmail.com>
2018-05-07 20:02:01 -0700

    Doing work

--- ticker-service/src/main/java/ticker/service/handler/TickerWebsocketHandler.java
+++ ticker-service/src/main/java/ticker/service/handler/TickerWebsocketHandler.java
@@ -1,12 +1,17 @@
 package ticker.service.handler;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.socket.WebSocketHandler;
 import org.springframework.web.reactive.socket.WebSocketSession;
 import reactor.core.publisher.Mono;
+import ticker.service.service.QuoteService;
 
 @Component
 public class TickerWebsocketHandler implements WebSocketHandler {
+
+    @Autowired
+    private QuoteService quoteService;
 
     @Override
     public Mono<Void> handle(WebSocketSession session) {
---
+++ ticker-service/src/main/java/ticker/service/service/QuoteService.java
@@ -0,0 +1,8 @@
+package ticker.service.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class QuoteService {
+
+}
