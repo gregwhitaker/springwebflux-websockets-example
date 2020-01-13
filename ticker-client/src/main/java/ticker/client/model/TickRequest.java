8550e40021c6f8427c0258af83e1b4b88c39d91f
gregwhitaker <gwhitake@gmail.com>
2020-01-12 16:22:21 -0800

    Doing work

---
+++ ticker-client/src/main/java/ticker/client/model/TickRequest.java
@@ -0,0 +1,14 @@
+package ticker.client.model;
+
+public class TickRequest {
+    
+    private String symbol;
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public void setSymbol(String symbol) {
+        this.symbol = symbol;
+    }
+}
