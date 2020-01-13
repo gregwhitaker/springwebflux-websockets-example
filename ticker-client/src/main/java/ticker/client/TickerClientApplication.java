aa786db0982c9ad07b404ed122781c6402ee5fd2
gregwhitaker <gwhitake@gmail.com>
2020-01-12 16:21:35 -0800

    Doing work

--- ticker-client/src/main/java/ticker/client/TickerClientApplication.java
+++ ticker-client/src/main/java/ticker/client/TickerClientApplication.java
@@ -2,14 +2,27 @@ package ticker.client;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import org.springframework.web.reactive.socket.WebSocketMessage;
+import org.springframework.web.reactive.socket.WebSocketSession;
+import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
+import org.springframework.web.reactive.socket.client.WebSocketClient;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.time.Duration;
 
 @SpringBootApplication
 public class TickerClientApplication {
 
     public static void main(String... args) throws Exception {
-        SpringApplication.run(TickerClientApplication.class);
+        SpringApplication app = new SpringApplication(TickerClientApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        app.run(args);
     }
 
     @Component
@@ -18,6 +31,16 @@ public class TickerClientApplication {
         @Override
         public void run(String... args) throws Exception {
 
+            WebSocketClient client = new ReactorNettyWebSocketClient();
+            client.execute(
+                    URI.create("ws://localhost:8080/tickers"),
+                    session -> session.send(
+                            Mono.empty())
+                            .thenMany(session.receive()
+                                    .map(WebSocketMessage::getPayloadAsText)
+                                    .log())
+                            .then())
+                    .block(Duration.ofSeconds(60L));
         }
     }
 }
